09c44a2d01bd780aee8a9cf59f5bfe491b22ee1a
Govindsing Chavan <123160733+govind2611@users.noreply.github.com>
2023-03-17 10:53:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 // complete the given function
 
 function palindrome(str){
- const reversedStr = str.split("").reverse().join("");
-  return str === reversedStr;
+// Convert the string to lowercase and remove non-alphanumeric characters
+  str = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+
+  // Compare the reversed string with the original string
+  return str === str.split('').reverse().join('');
 }
 module.exports = palindrome
